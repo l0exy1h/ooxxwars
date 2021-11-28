d99de57a20a0b1efdb7101c842bffbf100e46b92
Wang Ruochen <wrc@ruo-chen.wang>
2021-11-28 14:11:44 -0800

    Format Vies.hs

--- src/View.hs
+++ src/View.hs
@@ -1,14 +1,19 @@
-module View (view) where
+module View
+  ( view
+  ) where
 
-import Brick
-import Brick.Widgets.Center (center)
-import Brick.Widgets.Border (borderWithLabel, hBorder, vBorder)
-import Brick.Widgets.Border.Style (unicode)
-import Text.Printf (printf)
+import           Brick
+import           Brick.Widgets.Border           ( borderWithLabel
+                                                , hBorder
+                                                , vBorder
+                                                )
+import           Brick.Widgets.Border.Style     ( unicode )
+import           Brick.Widgets.Center           ( center )
+import           Text.Printf                    ( printf )
 
-import Model
-import Model.Board
-import Graphics.Vty hiding (dim)
+import           Graphics.Vty            hiding ( dim )
+import           Model
+import           Model.Board
 
 -------------------------------------------------------------------------------
 view :: PlayState -> [Widget String]
@@ -16,34 +21,27 @@ view :: PlayState -> [Widget String]
 view s = [view' s]
 
 view' :: PlayState -> Widget String
-view' s = 
-  withBorderStyle unicode $
-    borderWithLabel (str (header s)) $
-      vTile [ mkRow s row | row <- [1..dim] ]
+view' s = withBorderStyle unicode $ borderWithLabel (str (header s)) $ vTile
+  [ mkRow s row | row <- [1 .. dim] ]
 
 header :: PlayState -> String
 header s = printf "Tic-Tac-Toe Turn = %s, row = %d, col = %d" (show (psTurn s)) (pRow p) (pCol p)
-  where 
-    p    = psPos s
+  where p = psPos s
 
 mkRow :: PlayState -> Int -> Widget n
-mkRow s row = hTile [ mkCell s row i | i <- [1..dim] ]
+mkRow s row = hTile [ mkCell s row i | i <- [1 .. dim] ]
 
 mkCell :: PlayState -> Int -> Int -> Widget n
-mkCell s r c 
-  | isCurr s r c = withCursor raw 
-  | otherwise    = raw 
-  where
-    raw = mkCell' s r c
+mkCell s r c | isCurr s r c = withCursor raw
+             | otherwise    = raw
+  where raw = mkCell' s r c
 
 withCursor :: Widget n -> Widget n
 withCursor = modifyDefAttr (`withStyle` reverseVideo)
 
 mkCell' :: PlayState -> Int -> Int -> Widget n
 -- mkCell' _ r c = center (str (printf "(%d, %d)" r c))
-mkCell' s r c = center (mkXO xoMb)
-  where 
-    xoMb      = psBoard s ! Pos r c
+mkCell' s r c = center (mkXO xoMb) where xoMb = psBoard s ! Pos r c
     -- xoMb 
     --   | r == c    = Just X 
     --   | r > c     = Just O 
@@ -56,21 +54,14 @@ mkXO (Just O) = blockO
 
 blockB, blockX, blockO :: Widget n
 blockB = vBox (replicate 5 (str "     "))
-blockX = vBox [ str "X   X"
-              , str " X X "
-              , str "  X  "
-              , str " X X " 
-              , str "X   X"]
-blockO = vBox [ str "OOOOO"
-              , str "O   O"
-              , str "O   O"
-              , str "O   O"
-              , str "OOOOO"]
+blockX = vBox [str "X   X", str " X X ", str "  X  ", str " X X ", str "X   X"]
+blockO = vBox [str "OOOOO", str "O   O", str "O   O", str "O   O", str "OOOOO"]
 
 vTile :: [Widget n] -> Widget n
-vTile (b:bs) = vBox (b : [hBorder <=> b | b <- bs])
-vTile _      = emptyWidget
+vTile (b : bs) = vBox (b : [ hBorder <=> b | b <- bs ])
+vTile _        = emptyWidget
 
 hTile :: [Widget n] -> Widget n
-hTile (b:bs) = hBox (b : [vBorder <+> b | b <- bs])
-hTile _      = emptyWidget
+hTile (b : bs) = hBox (b : [ vBorder <+> b | b <- bs ])
+hTile _        = emptyWidget
+
